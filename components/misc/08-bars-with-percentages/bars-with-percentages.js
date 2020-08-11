026b6314a488c7941daa96e1e93267cbb758e1d1
Darby Dixon III <ddixon@oberlin.edu>
2020-08-11 10:41:24 -0400

    Some tweaks to the bars with percentages example.

--- components/misc/08-bars-with-percentages/bars-with-percentages.js
+++ components/misc/08-bars-with-percentages/bars-with-percentages.js
@@ -3,13 +3,13 @@
     console.log("bars-with-percentages active");
 
     const button = document.querySelector('#trigger');
-    const outputBox = document.querySelector('#outputBox');
+    // const outputBox = document.querySelector('#outputBox');
     const svgOutput = document.querySelector('#svgOutput');
 
     button.addEventListener('click', function() {
       const spinnerA = document.createElement('div');
       spinnerA.classList.add('spinner');
-      outputBox.appendChild(spinnerA);
+      // outputBox.appendChild(spinnerA);
 
       const spinnerB = document.createElement('div');
       spinnerB.classList.add('spinner');
@@ -18,7 +18,7 @@
       fetch('https://my.api.mockaroo.com/chart_data_2.json?count=10&key=0b401b10')
         .then(response => response.json())
         .then(data => {
-          outputBox.removeChild(spinnerA);
+          // outputBox.removeChild(spinnerA);
           svgOutput.removeChild(spinnerB);
 
           data.sort(function (x, y) {
@@ -37,8 +37,12 @@
           svg.attr("viewBox", [0, 0, w, h])
             .attr("preserveAspectRatio", "xMidYMid meet");
 
+          // const yScale = d3.scaleLinear()
+          //   .domain([0, d3.max(data, (d) => d.count)])
+          //   .range([h - padding, padding]);
+
           const yScale = d3.scaleLinear()
-            .domain([0, d3.max(data, (d) => d.count)])
+            .domain([0, 900])
             .range([h - padding, padding]);
 
           const xScale = d3.scaleLinear()
@@ -49,35 +53,18 @@
           console.log(xScale(5));
           console.log(xScale(10));
 
-          // yAxis - adding first so it appears below the bars?
-
-          const yAxis = d3.axisRight(yScale)
-            .tickSize(w - (padding * 1.25));
-
-          svg.append("g")
-            .attr("transform", "translate(" + padding + ", 0)")
-            .call(yAxis)
-            .call(g => g.selectAll(".tick:not(:first-of-type) line")
-              .attr("stroke-opacity", 0.5)
-              .attr("stroke-dasharray", "4,8"))
-            .call(g => g.selectAll(".tick line"))
-              .attr("stroke", "navy")
-            .call(g => g.selectAll(".tick text")
-              .attr("x", -24)
-              .attr("dy", -4));
-
           svg.selectAll("rect.bar")
             .data(data)
             .join('rect')
               .attr('class', 'bar')
               .attr('x', (d, i) => {
-                return i * ((w - (1.25 * padding)) / data.length) + padding;
+                return i * ((w - (1.25 * padding)) / data.length) + 1 + padding;
               })
               .attr("y", (d) => {
                 return yScale(d.count);
               })
               .attr("width", () => {
-                return ((w - (1.25 * padding)) / data.length) - 1;
+                return ((w - (1.25 * padding)) / data.length) - 4;
               })
               .attr("height", (d) => {
                 return yScale(0) - yScale(d.count);
@@ -97,7 +84,7 @@
                 return yScale(d.count);
               })
               .attr("width", () => {
-                return ((w - (1.25 * padding)) / data.length) - 1;
+                return ((w - (1.25 * padding)) / data.length) - 2;
               })
               .attr("height", (d) => {
                 return (yScale(0) - yScale(d.count)) * d.percentage;
@@ -121,6 +108,22 @@
               .style('font-family', 'Courier New')
               .style('fill', 'navy');
 
+          svg.selectAll("text.date")
+            .data(data)
+            .join("text")
+              .attr('class', 'date')
+              .text((d) => (d.date))
+              .attr('x', (d, i) => {
+                return i * ((w - (1.25 * padding)) / data.length) - 4 + padding + 10;
+              })
+              .attr("y", (d) => {
+                return h - 20;
+              })
+              .style('font-size', '0.75rem')
+              .style('font-weight', 'bold')
+              .style('font-family', 'Courier New')
+              .style('fill', 'navy');
+
 
           // xAxis
 
@@ -128,19 +131,42 @@
 
           svg.append("g")
             .attr("transform", "translate(0, " + (h - padding) + ")")
-            .call(xAxis);
+            .call(xAxis)
+            .call(g => {
+              console.dir(g);
+              g.selectAll(".tick text")
+                .attr("dy", "10000");
+            });
+
+          // yAxis - adding last so it appears above the bars
+
+          const yAxis = d3.axisRight(yScale)
+            .tickSize(w - (padding * 1.25));
+
+          svg.append("g")
+            .attr("transform", "translate(" + padding + ", 0)")
+            .call(yAxis)
+            .call(g => g.selectAll(".tick:not(:first-of-type) line")
+              .attr("stroke-opacity", 0.5)
+              .attr("stroke-dasharray", "4,8"))
+            .call(g => g.selectAll(".tick line"))
+            .attr("stroke", "navy")
+            .call(g => g.selectAll(".tick text")
+              .attr("x", -24)
+              .attr("dy", -4));
+
 
           // text version of results
 
-          data.forEach(function(item) {
-            let template = document.querySelector('#result_box');
-            let resultBox = document.importNode(template.content, true);
-
-            resultBox.querySelector('.date').innerHTML = item.date;
-            resultBox.querySelector('.count').innerHTML = item.count;
-
-            outputBox.appendChild(resultBox);
-          })
+          // data.forEach(function(item) {
+          //   let template = document.querySelector('#result_box');
+          //   let resultBox = document.importNode(template.content, true);
+          //
+          //   resultBox.querySelector('.date').innerHTML = item.date;
+          //   resultBox.querySelector('.count').innerHTML = item.count;
+          //
+          //   outputBox.appendChild(resultBox);
+          // })
         })
     })
   });
