8005a7b93e517d91d4b62bad10a0b06515f52d1f
Darby Dixon III <ddixon@oberlin.edu>
2020-08-19 08:27:42 -0400

    Some comments, and setting up the component update function.

--- iterateComponent.js
+++ iterateComponent.js
@@ -6,7 +6,8 @@ const prompt = require('prompt');
 
 const dir_path = path.join(__dirname, 'components');
 
-//passsing dir_path and callback function
+// Picking the directory of components
+
 fs.readdir(dir_path, {
   withFileTypes: true
 }, function (err, files) {
@@ -29,14 +30,12 @@ fs.readdir(dir_path, {
 
   console.log("Available component sets:")
   console.log("-------------------------")
-
   componentDirs.forEach(item => {
     console.log(item.option + " : " + item.directory);
   })
-
   console.log("-------------------------")
 
-  // prompt
+  // Prompt user for the directory to check
 
   prompt.message = "";
   prompt.start();
@@ -51,19 +50,17 @@ fs.readdir(dir_path, {
   });
 });
 
+// Given a directory, find the components inside it, pick one, and duplicate it
 
 function goDuplicateComponent(componentSetDir) {
   const dir_path = path.join(__dirname, 'components/', componentSetDir);
 
-//passsing dir_path and callback function
   fs.readdir(dir_path, {
     withFileTypes: true
   }, function (err, files) {
-    //handling error
     if (err) {
       return console.log('Unable to find or open the directory: ' + err);
     }
-
 
     const availableComponents = files.filter(dirent => dirent.isDirectory() && dirent.name !== 'node_modules')
       .map(dirent => dirent.name);
@@ -79,22 +76,17 @@ function goDuplicateComponent(componentSetDir) {
 
     console.log("Available components:")
     console.log("--------------------")
-
     availableComponentDirs.forEach(item => {
       console.log(item.option + " : " + item.directory);
     })
-
     console.log("--------------------")
 
-    // prompt
+    // Prompt user for the component to duplicate
 
     prompt.message = "";
     prompt.start();
 
-    prompt.get(['directory_to_duplicate', 'new_directory_name'], function(err, result) {
-      //
-      // Log the results.
-      //
+    prompt.get(['directory_to_duplicate', 'new_directory_name'], function (err, result) {
       console.log('Command-line input received:');
       console.log('  directory: ' + result.directory_to_duplicate);
       console.log('  new directory: ' + result.new_directory_name);
@@ -104,13 +96,28 @@ function goDuplicateComponent(componentSetDir) {
 
       console.log(dirPath + oldDir);
 
-      ncp(dirPath + oldDir, dirPath + result.new_directory_name, function(err) {
+      const oldComponent = dirPath + oldDir;
+      const newComponent = dirPath + result.new_directory_name;
+
+      ncp(oldComponent, newComponent, function (err) {
         if (err) {
           return console.error(err);
         }
-        console.log('done!');
+
+        console.log('finished duplicating component, updating new component next');
+        updateNewComponent(newComponent, result.new_directory_name);
       });
     });
   })
 }
 
+// Given a new component, update it
+function updateNewComponent(newComponent, newComponentName) {
+  console.log(newComponent);
+  console.log(newComponentName);
+  
+  // rename the files in the component
+
+  // edit the contents of the component
+
+}
