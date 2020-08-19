cc84fa5c2be54f3a0a144a9ce0443b9d16e613d0
Darby Dixon III <ddixon@oberlin.edu>
2020-08-19 13:57:01 -0400

    Line chart on top of a bar chart example.

---
+++ components/01-misc/19-bars-and-lines/bars-and-lines.js
@@ -0,0 +1,113 @@
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    console.log("bars-and-lines active");
+
+    // config
+
+    const width = 1000;
+    const height = 700;
+    const margin = {
+      "top": 20,
+      "bottom": 30,
+      "left": 40,
+      "right": 10
+    }
+
+    // data input and massaging
+
+    let datasetArray = [];
+
+    datasetArray.push({
+      id: 1,
+      count: dataset[0].number,
+      total: dataset[0].total
+    })
+
+    for (let i = 1; i < dataset.length; i++) {
+      datasetArray.push({
+        id: dataset[i].id,
+        count: dataset[i].number,
+        total: dataset[i].total
+      })
+    }
+
+    // scales
+
+    const xScale = d3.scaleBand()
+      .domain(datasetArray.map(d => d.id))
+      .range([margin.left, width - margin.right])
+      .padding(0.05);
+
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(datasetArray, (d) => d.total)])
+      .range([height - margin.bottom, margin.top]);
+
+    // svg output
+
+    const svg = d3.select("#svgOutput")
+      .append("svg")
+      .attr("viewBox", [0, 0, width, height])
+      .attr("preserveAspectRatio", "xMidYMid meet");
+
+    // add some bars
+
+    svg.selectAll("rect.bar")
+      .data(datasetArray)
+      .join('rect')
+      .attr('class', 'bar')
+      .attr('x', (d, i) => xScale(d.id))
+      .attr("y", (d) => yScale(d.total))
+      .attr("width", () => xScale.bandwidth())
+      .attr("height", (d) => yScale(0) - yScale(d.total))
+      .attr("data-total", (d) => (d.total))
+      .append('title')
+      .text((d) => d.date);
+
+
+    // create a line function and use it to draw a line
+
+    const line = d3.line()
+             .x(d => xScale(d.id))
+             .y(d => yScale(d.count))
+
+    svg.append('path')
+      .attr('d', line(datasetArray))
+      .attr('fill', 'none')
+      .attr('stroke', '#a6192e')
+      .attr('stroke-width', '1')
+      .attr('stroke-linejoin', 'round')
+      .attr('transform', 'translate(' + xScale.bandwidth() / 2 + ', 0)');
+
+    // plot the points as basic circles
+
+    svg.selectAll("circle.point")
+      .data(datasetArray)
+      .enter()
+      .append('circle')
+      .attr('class', 'point')
+      .attr('cx', (d) => xScale(d.id) + xScale.bandwidth() / 2)
+      .attr("cy", (d) => yScale(d.count))
+      .attr("r", '4')
+      .attr("fill", "white")
+      .attr("stroke", "#a6192e")
+      .attr("stroke-width", "0.5")
+      .append('title')
+      .text((d) => `id: ${d.id} / count: ${d.count}`);
+
+    // axes
+
+    const xAxis = d3.axisBottom(xScale)
+      .tickValues(xScale.domain().filter(function (d, i) {
+        return (i % 2)
+      }));
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+      .attr("transform", "translate(" + margin.left + ", 0)")
+      .call(yAxis);
+
+    svg.append("g")
+      .attr("transform", "translate(0, " + (height - margin.bottom) + ")")
+      .call(xAxis);
+  });
+})();
